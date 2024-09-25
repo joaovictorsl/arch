f62b7292e468f8c5aeecfd21163d3838e7a27ad4
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2024-09-25 14:05:11 -0300

    Remap: list diagnostics Config: autoformat on save

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,7 +3,8 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
+    lsp_zero.buffer_autoformat()
 end)
 
 local ensure_installed = {
@@ -32,7 +33,7 @@ require('mason-lspconfig').setup({
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
-local cmp_format = require('lsp-zero').cmp_format({details = true})
+local cmp_format = require('lsp-zero').cmp_format({ details = true })
 
 require('luasnip.loaders.from_vscode').lazy_load()
 
--- nvim/lua/jvsl/remap.lua
+++ nvim/lua/jvsl/remap.lua
@@ -1,9 +1,12 @@
+local telescope_builtin = require('telescope.builtin')
+
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, "NormalNC", {})
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>od', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>ld', telescope_builtin.diagnostics)
 vim.keymap.set('n', '<C-S-N>', function() vim.cmd('bn') end)
 vim.keymap.set('n', '<C-S-P>', function() vim.cmd('bp') end)
 vim.keymap.set('n', '<leader>bd', function() vim.cmd('bd') end)
