cea7e197bfe5aaa31fa6e76d0d4c0c7aadb2b45b
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2024-10-22 08:18:14 -0300

    Add: deno lsp Add: config filetype for docker compose files

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,6 +10,7 @@ end)
 local ensure_installed = {
     "bashls",
     "cssls",
+    "deno",
     "docker_compose_language_service",
     "dockerls",
     "gopls",
@@ -18,7 +19,6 @@ local ensure_installed = {
     "lua_ls",
     "pylyzer",
     "rust_analyzer",
-    "yamlls",
 }
 
 require('mason').setup({})
@@ -30,6 +30,17 @@ require('mason-lspconfig').setup({
         end,
     },
 })
+
+-- Handling filetypes
+local function set_filetype(pattern, filetype)
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = pattern,
+        command = "set filetype=" .. filetype,
+    })
+end
+
+set_filetype({ "docker-compose*.yaml", "docker-compose*.yml" }, "yaml.docker-compose")
+-- Handling filetypes
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
@@ -46,7 +57,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-.>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<Tab>'] = cmp_action.luasnip_supertab(),
