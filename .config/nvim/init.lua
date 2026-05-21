a46024a1160b2995b3707246b948feb528d3cf34
Joao Victor de Souza Lucena <joaovszlucena@gmail.com>
2026-05-21 00:55:58 -0300

    feat: update vimgrep default arguments on telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -426,11 +426,19 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden', -- Search hidden files
+            '--no-ignore', -- Don't respect .gitignore
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
